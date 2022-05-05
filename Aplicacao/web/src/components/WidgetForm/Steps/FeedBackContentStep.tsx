df4d9367c30053ab25b0c51196c5827bb3edbd1b
João Vitor Fonseca <jv.fonseca999@gmail.com>
2022-05-04 22:19:01 -0300

    Video aula 2 finalizando

--- Aplicacao/web/src/components/WidgetForm/Steps/FeedBackContentStep.tsx
+++ Aplicacao/web/src/components/WidgetForm/Steps/FeedBackContentStep.tsx
@@ -1,7 +1,42 @@
-export function FeedbackContentStep(){
+import { ArrowArcLeft, ArrowLeft } from "phosphor-react";
+import { FeedbackType, feedbackTypes } from ".."
+import { CloseButton } from "../../CloseButton"
+
+interface FeedbackContentStepProps {
+  feedbackType: FeedbackType;
+  onFeedbackRestartRequested: () => void;
+}
+
+export function FeedbackContentStep({feedbackType,onFeedbackRestartRequested}: FeedbackContentStepProps) {
+  const feedbackTypeData = feedbackTypes[feedbackType];
+
   return(
-    <div>
-      <p>hello world</p>
-    </div>
-  );
+    <>
+      <header>
+        <button 
+        type="button" 
+        className="top-5 left-2 absolute text-zinc-400 hover:text-zinc-100"
+        onClick={onFeedbackRestartRequested}>
+          <ArrowLeft weight="bold" className="h-4 w-4 "/>
+        </button>
+
+        <span className="text-xl leading-6 flex items-center gap-2">
+          <img className="h-6 w-6" src={feedbackTypeData.image.source} alt={feedbackTypeData.image.alt}/>
+          {feedbackTypeData.title} 
+        </span>
+
+        <CloseButton/> 
+      </header>
+    
+      <form className="my-3 w-full ">
+        <textarea
+        className="min-w-[304px] w-full min-h-[112px] h-auto text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md  hover:border focus:border-brand-500 focus:ring-brand-500 focus:ring-3 resize-none focus:outline-none"
+        placeholder="Conte com detalhes o que está acontecendo..."
+        >
+
+        </textarea>
+      </form>
+        
+    </>  
+  )
 }
--- Aplicacao/web/src/components/WidgetForm/index.tsx
+++ Aplicacao/web/src/components/WidgetForm/index.tsx
@@ -4,6 +4,7 @@ import ideaImageUrl from "../../assets/idea.png";
 import bugImageUrl from "../../assets/bug.png";
 import { useState } from "react";
 import { FeedbackTypeStep } from "./Steps/FeedBackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedBackContentStep";
 
 
 
@@ -35,32 +36,25 @@ export const feedbackTypes = {
 export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm(){
+
+  function handleRestartFeedback(){
+    setFeedbackType(null);
+  }
   
   const[feedbackType,setFeedbackType] = useState<FeedbackType|null>(null);
 
   return (
-    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 items-center flex-col shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      <header>
-        <span className="text-xl leading-6">Deixe seu Feedback </span>
-        <CloseButton/> 
-      </header>
-      
-
-
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex items-center flex-col shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       
       {!feedbackType ? (
         <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
       ):(
-        <p>hello world</p>
+        <FeedbackContentStep  feedbackType={feedbackType} 
+                              onFeedbackRestartRequested={handleRestartFeedback}/>
+        
       )}
 
-      
-
-
-
-
-
-      <footer className="text-xs text-neutral-400">
+      <footer className="text-xs text-neutral-400 align-middle">
         Feito com ♥ - <a className="underline underline-offset-2" href="https://github.com/Joaovitron999">Joaovitron999</a>
       </footer>
     </div>
